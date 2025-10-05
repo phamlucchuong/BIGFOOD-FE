315b8256883d49ba7139c664aa3b01f6264760ce
minhthang12 <6151071102@st.utc2.edu.vn>
2025-10-05 22:59:55 +0700

    feat: ui cua trang pickfood

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css';
 import "react-phone-input-2/lib/style.css";
 import ScreenRouter from './router/ScreenRouter.jsx';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home/Home.jsx'; 
+import Pickfood from './pages/pickfood/pickfood.jsx';
 
 function App() {
   
-  return <ScreenRouter />;
+  // return <ScreenRouter />;
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Trang chủ</Link>
+        <Link to="/Pickfood">Pickfood</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Pickfood" element={<Pickfood />} /> 
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
