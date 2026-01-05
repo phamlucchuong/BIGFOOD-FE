f41ac154b81e12ad93ad14630e7a34d6b5e6ca3f
phamlucchuong <lucchuongg@gmail.com>
2026-01-06 04:29:42 +0700

    fix: bug

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 // Tự động nhận diện môi trường: nếu có biến VITE_API_URL thì dùng, không thì dùng localhost
-export const API_BASE_URL = import.meta.env.VITE_API_URL || '${API_BASE_URL}';
+export const API_BASE_URL = import.meta.env.API_BASE_URL || '${API_BASE_URL}';
